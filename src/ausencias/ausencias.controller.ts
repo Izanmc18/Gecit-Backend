f4a0b478ea8c8b1e179a8475fbdbc29b8a3c5319
Izan Martinez <imc001.edu@innovasur.com>
2026-03-20 14:15:42 +0100

    feat(security): applying permissions to appointment and absence controllers

--- src/ausencias/ausencias.controller.ts
+++ src/ausencias/ausencias.controller.ts
@@ -11,30 +11,36 @@ import {
 import { AusenciasService } from './ausencias.service';
 import { CreateAusenciaDto, UpdateAusenciaDto } from './dto';
 import { AusenciaAdapter } from './adapters/ausencia.adapter';
+import { Auth } from '../auth/decorators';
+import { ValidRoles } from '../auth/interfaces';
 
 @Controller('ausencias')
 export class AusenciasController {
   constructor(private readonly ausenciasService: AusenciasService) {}
 
   @Post()
+  @Auth(ValidRoles.admin, ValidRoles.empleado)
   async create(@Body() createAusenciaDto: CreateAusenciaDto) {
     const ausencia = await this.ausenciasService.create(createAusenciaDto);
     return AusenciaAdapter.toResponse(ausencia);
   }
 
   @Get()
+  @Auth(ValidRoles.admin, ValidRoles.empleado)
   async findAll() {
     const ausencias = await this.ausenciasService.findAll();
     return AusenciaAdapter.toResponseList(ausencias);
   }
 
   @Get(':id')
+  @Auth(ValidRoles.admin, ValidRoles.empleado)
   async findOne(@Param('id', ParseUUIDPipe) id: string) {
     const ausencia = await this.ausenciasService.findOne(id);
     return AusenciaAdapter.toResponse(ausencia);
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.admin, ValidRoles.empleado)
   async update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateAusenciaDto: UpdateAusenciaDto,
@@ -44,6 +50,7 @@ export class AusenciasController {
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.admin, ValidRoles.empleado)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.ausenciasService.remove(id);
   }
--- src/citas/citas.controller.ts
+++ src/citas/citas.controller.ts
@@ -11,30 +11,36 @@ import {
 import { CitasService } from './citas.service';
 import { CreateCitaDto, UpdateCitaDto } from './dto';
 import { CitaAdapter } from './adapters/cita.adapter';
+import { Auth } from '../auth/decorators';
+import { ValidRoles } from '../auth/interfaces';
 
 @Controller('citas')
 export class CitasController {
   constructor(private readonly citasService: CitasService) {}
 
   @Post()
+  @Auth(ValidRoles.admin, ValidRoles.empleado)
   async create(@Body() createCitaDto: CreateCitaDto) {
     const cita = await this.citasService.create(createCitaDto);
     return CitaAdapter.toResponse(cita);
   }
 
   @Get()
+  @Auth(ValidRoles.admin, ValidRoles.empleado)
   async findAll() {
     const citas = await this.citasService.findAll();
     return CitaAdapter.toResponseList(citas);
   }
 
   @Get(':id')
+  @Auth(ValidRoles.admin, ValidRoles.empleado)
   async findOne(@Param('id', ParseUUIDPipe) id: string) {
     const cita = await this.citasService.findOne(id);
     return CitaAdapter.toResponse(cita);
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.admin, ValidRoles.empleado)
   async update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateCitaDto: UpdateCitaDto,
@@ -44,6 +50,7 @@ export class CitasController {
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.admin, ValidRoles.empleado)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.citasService.remove(id);
   }
