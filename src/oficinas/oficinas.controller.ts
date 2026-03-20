047c22ce4bf8bc0096f05333a410c4d1d1968742
Izan Martinez <imc001.edu@innovasur.com>
2026-03-20 14:27:42 +0100

    feat(security): I apply read-only permissions for employees in catalogs and configuration

--- src/oficinas/oficinas.controller.ts
+++ src/oficinas/oficinas.controller.ts
@@ -10,27 +10,33 @@ import {
 } from '@nestjs/common';
 import { OficinasService } from './oficinas.service';
 import { CreateOficinaDto, UpdateOficinaDto } from './dto';
+import { Auth } from '../auth/decorators';
+import { ValidRoles } from '../auth/interfaces';
 
 @Controller('oficinas')
 export class OficinasController {
   constructor(private readonly oficinasService: OficinasService) {}
 
   @Post()
+  @Auth(ValidRoles.admin)
   create(@Body() createOficinaDto: CreateOficinaDto) {
     return this.oficinasService.create(createOficinaDto);
   }
 
   @Get()
+  @Auth(ValidRoles.admin, ValidRoles.empleado)
   findAll() {
     return this.oficinasService.findAll();
   }
 
   @Get(':id')
+  @Auth(ValidRoles.admin, ValidRoles.empleado)
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.oficinasService.findOne(id);
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.admin)
   update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateOficinaDto: UpdateOficinaDto,
@@ -39,6 +45,7 @@ export class OficinasController {
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.admin)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.oficinasService.remove(id);
   }
