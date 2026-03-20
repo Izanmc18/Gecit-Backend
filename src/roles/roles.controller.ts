50c690bac049de6b1facd2f69b68558aa1ba145d
Izan Martinez <imc001.edu@innovasur.com>
2026-03-20 14:03:57 +0100

    feat(auth): implement global JwtAuthGuard and open login route with @Public decorator

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -11,30 +11,36 @@ import {
 import { RolesService } from './roles.service';
 import { CreateRolDto, UpdateRolDto } from './dto';
 import { RolAdapter } from './adapters/rol.adapter';
+import { Auth } from '../auth/decorators';
+import { ValidRoles } from '../auth/interfaces';
 
 @Controller('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
   @Post()
+  @Auth(ValidRoles.admin)
   async create(@Body() createRolDto: CreateRolDto) {
     const rol = await this.rolesService.create(createRolDto);
     return RolAdapter.toResponse(rol);
   }
 
   @Get()
+  @Auth(ValidRoles.admin)
   async findAll() {
     const roles = await this.rolesService.findAll();
     return RolAdapter.toResponseList(roles);
   }
 
   @Get(':id')
+  @Auth(ValidRoles.admin)
   async findOne(@Param('id', ParseUUIDPipe) id: string) {
     const rol = await this.rolesService.findOne(id);
     return RolAdapter.toResponse(rol);
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.admin)
   async update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateRolDto: UpdateRolDto,
@@ -44,6 +50,7 @@ export class RolesController {
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.admin)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.rolesService.remove(id);
   }
--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -11,30 +11,36 @@ import {
 import { UsuariosService } from './usuarios.service';
 import { CreateUsuarioDto, UpdateUsuarioDto } from './dto';
 import { UsuarioAdapter } from './adapters/usuario.adapter';
+import { Auth } from '../auth/decorators';
+import { ValidRoles } from '../auth/interfaces';
 
 @Controller('usuarios')
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) {}
 
   @Post()
+  @Auth(ValidRoles.admin)
   async create(@Body() createUsuarioDto: CreateUsuarioDto) {
     const usuario = await this.usuariosService.create(createUsuarioDto);
     return UsuarioAdapter.toResponse(usuario);
   }
 
   @Get()
+  @Auth(ValidRoles.admin)
   async findAll() {
     const usuarios = await this.usuariosService.findAll();
     return UsuarioAdapter.toResponseList(usuarios);
   }
 
   @Get(':id')
+  @Auth(ValidRoles.admin, ValidRoles.empleado)
   async findOne(@Param('id', ParseUUIDPipe) id: string) {
     const usuario = await this.usuariosService.findOne(id);
     return UsuarioAdapter.toResponse(usuario);
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.admin, ValidRoles.empleado)
   async update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateUsuarioDto: UpdateUsuarioDto,
@@ -44,6 +50,7 @@ export class UsuariosController {
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.admin)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.usuariosService.remove(id);
   }
